160079a62bc8ac36a0e240fce4b09631b93d7f0f
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-09-01 21:48:09 +0300

    Add

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,23 @@
-let country = 'Bulgaria';
-let continent = 'Europe';
+const country = 'Bulgaria';
+const continent = 'Europe';
 let population = 6.8;
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+const isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+language = 'Bulgarian';
+
+console.log(population / 2);
+console.log(population + 1);
+console.log(population > 6);
+console.log(population < 33);
+
+const description = 'Portugal is in Europe, and its 11 million people speak portuguese';
