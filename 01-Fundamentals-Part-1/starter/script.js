4d739e8a8a0f2a08ccc200fce819a35191854a62
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-09-02 00:07:28 +0300

    Add

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -4,6 +4,18 @@ console.log(40 + 15 + 10 + 60 - 10);
 console.log('Jonas');
 console.log(23);
 
-let firstName = 'Jonas';
-console.log(firstName);
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2023;
 
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(jonasNew);
+
+console.log('Strings with \n\ multiple \n\ lines');
+console.log(`Strings with
+multiple
+lines`);
