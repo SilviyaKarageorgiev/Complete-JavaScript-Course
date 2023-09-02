e9af7c8474820cb822f0c86256c69f7c8005f0a2
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2023-09-02 21:21:23 +0300

    Add

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -30,3 +30,32 @@ if (isOldEnough) {
     console.log(`Sara is too young. Wait another ${yearsLeft} years 😉`);
 }
 
+const day = 'sunday';
+
+switch (day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare thory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend');
+        break;
+    default:
+        console.log('Not a valid day!');
+}
+
+const anotherAge = 23;
+age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+
+const drink = age >= 18 ? 'wine' : 'water';
